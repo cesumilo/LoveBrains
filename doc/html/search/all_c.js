d40db0d2367205b2d17d67ab58bed8337849b1ab
Guillaume ROBIN <guillaume1.robin@epitech.eu>
2015-08-26 16:56:10 +0200

    Update: doxygen documentation

--- doc/html/search/all_c.js
+++ doc/html/search/all_c.js
@@ -10,5 +10,5 @@ var searchData=
   ['logicerror',['LogicError',['../class_json_1_1_logic_error.html',1,'Json']]],
   ['lovebrains_20_2d_20api_20description',['LoveBrains - API Description',['../md__home_robin_f__programming__git__c_p_p__love_brains_api__r_e_a_d_m_e.html',1,'']]],
   ['lovebrains_20_2d_20api_20description',['LoveBrains - API Description',['../md__home_robin_f__programming__git__c_p_p__love_brains_mods_basic_survival_api__r_e_a_d_m_e.html',1,'']]],
-  ['lovebrains',['LoveBrains',['../md__home_robin_f__programming__git__c_p_p__love_brains__r_e_a_d_m_e.html',1,'']]]
+  ['lovebrains_20_28beta_201_2e0_2e0_29',['LoveBrains (Beta 1.0.0)',['../md__home_robin_f__programming__git__c_p_p__love_brains__r_e_a_d_m_e.html',1,'']]]
 ];
